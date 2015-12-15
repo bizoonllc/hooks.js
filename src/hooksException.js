4edfabb6fe7825bc4d93d0bf90c74aadf426cc9b
Jerzy Ogonowski <jerzy.dominik.ogonowski@gmail.com>
2015-12-15 01:01:44 +0000

    Added exception-factory dependency

--- src/hooksException.js
+++ src/hooksException.js
@@ -1,15 +1,2 @@
-function hooksException() {
-    var temp = Error.apply(this, arguments);
-    temp.name = this.name = 'hooksException';
-    this.stack = temp.stack;
-    this.message = 'hooks exception: ' + temp.message;
-}
-hooksException.prototype = Object.create(Error.prototype, {
-    constructor: {
-        value: hooksException,
-        writable: true,
-        configurable: true
-    }
-});
-
-module.exports = hooksException;
+var exceptionFactory = require('exceptionFactory');
+module.exports = new exceptionFactory('hooksException', 'hooks exception: ');
